ab4660ff1f6c3d110f9a2b1896cca9914501fc6f
Kyle <slharris@hmc.edu>
2014-07-14 16:27:59 -0700

    Don't know how to optimize Cypher enough to run. Started building other test cases.

--- src/main/java/org/monarch/sim/CypherTraversals.java
+++ src/main/java/org/monarch/sim/CypherTraversals.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Map;
 
 import org.neo4j.cypher.javacompat.ExecutionEngine;
-import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.Node;
 
 public class CypherTraversals {
--- src/test/java/org/monarch/sim/CypherTest.java
+++ src/test/java/org/monarch/sim/CypherTest.java
@@ -14,10 +14,14 @@ public class CypherTest {
 	// Complete graph.
 	static GraphDatabaseService completeDB;
 	static ExecutionEngine completeEngine;
+	// Balanced binary tree.
+	static GraphDatabaseService treeDB;
+	static ExecutionEngine treeEngine;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		buildCompleteDB(6);
+		buildCompleteDB(16);
+		buildTreeDB(15);
 	}
 
 	private static void buildCompleteDB(int i) {
@@ -45,17 +49,40 @@ public class CypherTest {
 		completeEngine.execute(query);
 	}
 
+	// FIXME: This doesn't work.
+	private static void buildTreeDB(int i) {
+		treeDB = new TestGraphDatabaseFactory().newImpermanentDatabase();
+		treeEngine = new ExecutionEngine(treeDB);
+		// Create a root.
+		String query = "CREATE (root {count:0}) ";
+		// Build the right number of nodes.
+		query += "FOREACH (";
+		query += "x in range(1, " + i + ") ";
+		query += "| ";
+		query += "CREATE (leaf {count:x}) ";
+		query += ") ";
+		// Connect nodes in the correct direction.
+		query += "MATCH (a), (b) ";
+		query += "WHERE (id(a) = 2 * id(b) OR id(a) = 2 * id(b) + 1) ";
+		query += "CREATE (a)-[:X]->(b) ";
+		query += "WITH a, b ";
+		
+		treeEngine.execute(query);
+	}
+
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		// Clean up all our graphs.
 		completeDB.shutdown();
+		treeDB.shutdown();
 	}
 
 	@Test
 	public void test() {
-		Iterable<Node> nodes = GlobalGraphOperations.at(completeDB).getAllNodes();
+		Iterable<Node> nodes = GlobalGraphOperations.at(treeDB).getAllNodes();
 		for (Node n : nodes)
 		{
-			System.out.println(CypherTraversals.getAncestors(n, completeEngine));
+			System.out.println(CypherTraversals.getAncestors(n, treeEngine));
 		}
 		for (Node first : nodes)
 		{
@@ -64,7 +91,7 @@ public class CypherTest {
 			{
 				long secondId = second.getId();
 				System.out.println(firstId + " " + secondId + ":");
-				System.out.println(CypherTraversals.getCommonAncestors(first, second, completeEngine));
+				System.out.println(CypherTraversals.getCommonAncestors(first, second, treeEngine));
 			}
 		}
 	}
