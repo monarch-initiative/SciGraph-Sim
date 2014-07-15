c709313ffd19e5292c594a7b25593845d2df139d
Christopher Condit <condit@sdsc.edu>
2014-07-15 11:26:02 -0700

    Adds an OWL loading utility for testing.

---
+++ src/test/java/org/monarch/sim/OwlTestUtil.java
@@ -0,0 +1,25 @@
+package org.monarch.sim;
+
+import edu.sdsc.scigraph.neo4j.OntologyConfiguration;
+import edu.sdsc.scigraph.owlapi.OwlLoadConfiguration;
+import edu.sdsc.scigraph.owlapi.OwlLoader;
+
+public class OwlTestUtil {
+
+  /***
+   * Load an OWL import chain.
+   * 
+   * @param url The root URL of the import chain ("http://www.w3.org/TR/owl-guide/wine.rdf")
+   * @param graphLocation The location on disk to store the graph ("target/wine" might be good for
+   *        testing)
+   */
+  public static void loadOntology(String url, String graphLocation) {
+    OntologyConfiguration ontologyConfiguration = new OntologyConfiguration();
+    ontologyConfiguration.setGraphLocation(graphLocation);
+    OwlLoadConfiguration config = new OwlLoadConfiguration();
+    config.setOntologyConfiguration(ontologyConfiguration);
+    config.getOntologyUrls().add(url);
+    OwlLoader.load(config);
+  }
+
+}
